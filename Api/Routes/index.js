aee88ad094e116ff577f0d504086ff7fb63d72df
Luiz Kota <luiz.zk@gmail.com>
2017-08-16 22:47:17 -0300

    Reorganizing the routes structure

--- Api/Routes/index.js
+++ Api/Routes/index.js
@@ -1,8 +1,7 @@
+// Import healthcheck routes
+const HealthcheckRoutes = require('./healthcheck.js');
+
 module.exports = (api) => {
-    api.get('/healthcheck/', (req, res) => {
-        res.json({
-            status: true,
-            message: 'API ok!'
-        });
-    });
+    //Register healthcheck routes
+    HealthcheckRoutes(api);
 }
